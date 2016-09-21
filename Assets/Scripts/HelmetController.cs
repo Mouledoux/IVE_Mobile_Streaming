92b5f37aef1bccf6b5238e72fb12506d0b18e76b
Eric Mouledoux <EricMouledoux@gmail.com>
2016-09-20 21:32:39 -0500

    Timer delay

--- Assets/Scripts/HelmetController.cs
+++ Assets/Scripts/HelmetController.cs
@@ -5,21 +5,29 @@ public class HelmetController : MonoBehaviour
 {
     public GameObject IntroScreen;
     public _8222016 VideoPlayer;
-	
+
+    private float timer = 0;
+    private float delay = 10;
+
 	void Update ()
     {
-	    if(Input.GetKeyUp(KeyCode.F7))
+	    if(timer >= delay)//Input.GetKeyUp(KeyCode.F7))
         {
             if(IntroScreen.activeSelf)
             {
                 IntroScreen.SetActive(false);
                 VideoPlayer.enabled = true;
+                VideoPlayer.PlayPause();
             }
-
-            VideoPlayer.PlayPause();
         }
+        else
+        {
+            timer += Time.deltaTime;
+        }
+
         if(VideoPlayer.GetCurrentVideoMPCState() == MediaPlayerCtrl.MEDIAPLAYER_STATE.END)
         {
+            timer = 0;
             IntroScreen.SetActive(true);
         }
 
